51c48286c0b0166162a9eac96f0751a8ad05bb91
Daniel Ferreira da Costa Pinto <91293389+danielfcp190@users.noreply.github.com>
2022-04-01 08:53:50 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
