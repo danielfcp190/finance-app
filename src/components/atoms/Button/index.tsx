ef1db268ab64c0dbd950443f72bb8d7461322ba4
Daniel Ferreira da Costa Pinto <91293389+danielfcp190@users.noreply.github.com>
2022-04-01 11:05:30 -0300

    add ghost button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,16 +1,26 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
+import tw from "tailwind-styled-components";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  variant?: "ghost";
 }
 
-export default function Button({ children, ...props }: Props) {
-  return (
-    <button
-      {...props}
-      className="bg-purple-500 h-10 text-white font-bold hover:bg-purple-400 disabled:cursor-not-allowed disabled:bg-gray-200"
-    >
-      {children}
-    </button>
-  );
+export default function Button({ children, variant, ...props }: Props) {
+  if (variant === "ghost") {
+    return <GhostButton {...props}>{children}</GhostButton>;
+  }
+  return <MainButton {...props}>{children}</MainButton>;
 }
+
+export const BaseButton = tw.button`
+ h-10 px-8 text-white font-bold  disabled:cursor-not-allowed disabled:bg-gray-200
+`;
+
+export const MainButton = tw(BaseButton)`
+bg-purple-500 hover:bg-purple-400
+`;
+
+export const GhostButton = tw(BaseButton)`
+bg-transparent hover:bg-purple-100 text-purple-500
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,16 @@
+import Button from "components/atoms/Button";
+import { TemplateContainer } from "components/templates";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <TemplateContainer>
+      <header className="w-full max-w-screen-lg flex justify-between items-center h-1/6">
+        <span className="font-bold text-2xl">Expense tracker</span>
+        <nav className="flex ">
+          <Button>Add new expense</Button>
+          <Button variant="ghost">Logout</Button>
+        </nav>
+      </header>
+    </TemplateContainer>
+  );
 }
